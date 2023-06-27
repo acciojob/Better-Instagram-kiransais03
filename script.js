1a9b1b4e986fbedca8cc5e5bc878ec728e50b2af
Guduru Kiran Sai <119746361+kiransais03@users.noreply.github.com>
2023-06-27 16:03:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your code here
+function allowDrop(even) {
+  even.preventDefault();
+}
+
+function drag(even) {
+  even.dataTransfer.setData("text", even.target.id);
+}
+
+function drop(even) {
+  even.preventDefault();
+  const fetchData = even.dataTransfer.getData("text");
+  even.target.appendChild(document.getElementById(fetchData));
+}
